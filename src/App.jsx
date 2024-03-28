c2001c02ccc5f34a69bafd2cee2a81bd980baa56
Jamshaid <jamshaidjavaid360@gmail.com>
2024-03-29 02:54:37 +0500

    pushing code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./Form";
+import HomePage from "./HomePage";
+
+function App() {
+  return (
+    <div className="app-main-container">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Form.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./Form.css";
+
+const initialState = {
+  name: "",
+  email: "",
+  phoneNo: "",
+  address: "",
+};
+
+const Form = () => {
+  const [formValues, setFormValues] = useState(initialState);
+
+  const inputChangeHandler = (event) => {
+    console.log(event.target.name);
+    setFormValues({ ...formValues, [event.target.name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(formValues);
+
+    console.log("submitted");
+  };
+
+  return (
+    <div className="form-main-container">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Enter name"
+          onChange={inputChangeHandler}
+          value={formValues.name}
+        />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter email"
+          onChange={inputChangeHandler}
+          value={formValues.email}
+        />
+        <input
+          type="number"
+          name="phoneNo"
+          id="phoneNo"
+          placeholder="Enter phone number"
+          onChange={inputChangeHandler}
+          value={formValues.phoneNo}
+        />
+        <input
+          type="text"
+          name="address"
+          id="address"
+          placeholder="Enter address"
+          onChange={inputChangeHandler}
+          value={formValues.address}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
