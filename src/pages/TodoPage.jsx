950747b279d7a27a93f614bf75afcdcb6abe0108
Aliye ilbay <yazm921@gmail.com>
2024-04-01 23:20:47 +0200

    Todos

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -8,23 +8,44 @@ import "./TodoPage.scss";
 const TodoPage = () => {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
+  const [name, setName] = useState("");
+  const [time, setTime] = useState("");
 
   const handleInputChange = (event) => {
     setTodo(event.target.value);
   };
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
 
+  const handleTimeChange = (event) => {
+    setTime(event.target.value);
+  };
+
+  
   const onSubmit = (e) => {
     e.preventDefault();
-    if (!todo) return;
+    if (!name || !time) return;
 
-    setTodos([...todos, todo]);
+    setTodos([...todos,{todo,name,time}]);
     setTodo("");
+    setName("");
+    setTime("");
+
   };
 
   return (
     <div className="todo-page-container">
       <div className="container">
         <form onSubmit={onSubmit}>
+        <input 
+        name="Name" 
+        id="Name" 
+        placeholder="Enter your name" 
+        type="text"
+        value={name}
+        onChange={handleNameChange } />
+        
           <input
             name="todo"
             id="todo"
@@ -32,14 +53,23 @@ const TodoPage = () => {
             type="text"
             value={todo}
             onChange={handleInputChange}
-          />
+            />
+            <input
+            name="time" 
+            id="time" 
+            type="time"
+            value={time}
+            onChange={handleTimeChange}
+            />
           <button>Submit</button>
         </form>
         <div className="all-todos-container">
           <h4>My All Todos</h4>
           {todos.map((item) => (
-            <div className="single-item">
-              <h6>{item}</h6>
+            <div key={item} className="single-item">
+            <h6>{item.todo}</h6>
+              <p>Name: {item.name}</p>
+              <p>Time: {item.time}</p>
             </div>
           ))}
         </div>
