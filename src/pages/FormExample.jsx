db6ed998c59f03ecb2c4e38ac46266b811e7affb
Aliye ilbay <yazm921@gmail.com>
2024-04-24 16:03:43 +0200

    deneme

--- src/pages/FormExample.jsx
+++ src/pages/FormExample.jsx
@@ -25,20 +25,29 @@ const FormExample = () => {
 
 
         const validateForm = () => {
+            //! Fonksiyonun başında isValid ve errorsCopy değişkenleri tanımlanıyor.aslinda errorsCopy orjinal errorsi bozmamak icin yeni errorlari buraya eklemek icin olusturuluyor.
+
             let isValid = true;
             let errorsCopy = { ...errors };
         
+            //! formData'nın her bir öğesi üzerinde döngü oluşturuluyor.
             Object.entries(formData).forEach((el) => {
+                //!Eğer formData'nın bir öğesi boş veya false değerine sahipse,
                 if (!el[1]) {
-                  errorsCopy = { ...errorsCopy, [el[0]]: `Please write ${el[0]}!` };
-                  isValid = false;
+                    //! errorsCopy objesine yeni bir hata mesajı ekleniyor.
+                    //! Örneğin, formData'nın bir öğesi 'name' ise, hata mesajı 'Please write name!' olacak.
+                    errorsCopy = { ...errorsCopy, [el[0]]: `Please write ${el[0]}!` };
+                    //! isValid değeri false olarak güncelleniyor.
+                    isValid = false;
                 }
-              });
+            });
         
+            //! errorsCopy objesini state'teki errors objesine atanıyor.
             setErrors(errorsCopy);
         
+            //! isValid değeri fonksiyon sonucu olarak döndürülüyor.
             return isValid;
-          };
+        };
 
 
     const FormSubmit=(e)=>{
